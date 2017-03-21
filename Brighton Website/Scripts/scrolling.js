fb2a966fad396d4224cf764b2cfb4b581e304c0d
Mihaly <mihaly_kisvarday@yahoo.com>
2017-03-21 23:48:12 +0000

    Learning Bootstrap

---
+++ Brighton Website/Scripts/scrolling.js
@@ -0,0 +1,31 @@
+﻿
+$(function () {
+
+    //set div height for background image
+    $(window).on("load resize", function () {
+        $(".fill-screen").css("height", window.innerHeight);
+    });
+
+    // add Bootstrap's scrollspy
+    $('body').scrollspy({
+        target: '.navbar',
+        offset: 160
+    });
+
+    // smooth scrolling
+    $('nav a, .down-button a').bind('click', function () {
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top - 80
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    //parallax scrolling
+    //$(window).stellar();
+
+    //initalize WOW
+    new WOW().init();
+
+});
+    
+    
